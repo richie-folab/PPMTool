522471be3cba2f8170c71f943cfc6261d17a67a4
richie-folab <richie_folab@yahoo.com>
2021-12-27 08:19:01 +0100

    Added Header component

--- ppmtool-react-app/src/components/Dashboard.js
+++ ppmtool-react-app/src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import Header from "./Layout/Header";
+import ProjectItem from "./Project/ProjectItem";
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the dashboard</h1>
+        <Header />
+        <h1>Welecome to the Dashboard</h1>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
       </div>
     );
   }
