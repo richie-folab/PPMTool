b69ee0e86ca3e834ec68c65411924039be1c561a
richie-folab <richie_folab@yahoo.com>
2021-12-27 14:55:26 +0100

    bootstrap 5 installed and added to app.js

--- ppmtool-react-app/src/components/Dashboard.js
+++ ppmtool-react-app/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Header from "./Layout/Header";
 import ProjectItem from "./Project/ProjectItem";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
         <Header />
-        <h1>Welecome to the Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
         <ProjectItem />
         <ProjectItem />
         <ProjectItem />
