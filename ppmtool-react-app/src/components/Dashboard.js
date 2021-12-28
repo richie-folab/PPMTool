88c6565aea793901dc53214a6cfdf41042b18e11
richie-folab <richie_folab@yahoo.com>
2021-12-28 23:31:43 +0100

    Added Jsx to Dashboard, Header and ProjectItem components

--- ppmtool-react-app/src/components/Dashboard.js
+++ ppmtool-react-app/src/components/Dashboard.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
-import Header from "./Layout/Header";
 import ProjectItem from "./Project/ProjectItem";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+export default Dashboard;
