c1276c3b0c51ec01a585d20715050b445c952619
richie-folab <richie_folab@yahoo.com>
2021-12-29 16:30:09 +0100

    Added addProject component and React Router

--- ppmtool-react-app/src/App.js
+++ ppmtool-react-app/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import AddProject from "./components/Project/AddProject";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
---
+++ ppmtool-react-app/src/components/Project/AddProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AddProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Project</h1>
+      </div>
+    );
+  }
+}
